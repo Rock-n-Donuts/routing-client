7365042e59abc77bb2ea18b5bc41b3ab4aa0b775
Martin Hamel <martin@komunide.com>
2023-03-09 19:24:16 +0000

    now using the browser position as start

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { TileLayer, Marker, Popup, useMapEvents, Polyline } from 'react-leaflet'
-import { LatLngExpression, LeafletMouseEvent } from 'leaflet';
+import { LatLng, LeafletMouseEvent } from 'leaflet';
 import useRoute from './useRoute';
 
 const App = () => {
   const [route, setCoord] = useRoute();
+  const [start, setStart] = useState<LatLng>({ lat: 45.4620335, lng: -73.5850831 } as LatLng);
+  const [end, setend] = useState<LatLng>({ lat: 45.4620335, lng: -73.5850831 } as LatLng);
   useMapEvents({
     click: (e: LeafletMouseEvent) => {
-      setCoord(e.latlng);
-    }
+      setend(e.latlng);
+      setCoord({ start: start, end: e.latlng });
+    },
+    locationfound(e) {
+      setStart(e.latlng);
+      setCoord({ start: e.latlng, end: end });
+    },
   });
 
 
   return (
     <div>,
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
       />
-      <Marker position={[45.4620335, -73.5850831]}>
+      <Marker position={[start.lat, start.lng]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
-      <Polyline positions={route}/>
+      <Polyline positions={route} color="orange" weight={6} />
     </div>
   );
 }
--- src/useRoute.ts
+++ src/useRoute.ts
@@ -3,14 +3,18 @@ import { LatLng, LatLngExpression } from "leaflet";
 import { useEffect, useState } from "react";
 
 const useRoute = () => {
-  const [coord, setCoord] = useState<LatLng | null>(null);
+  const [coord, setCoord] = useState<{ start: LatLng, end: LatLng } | null>(
+    null
+  );
   const [route, setRoute] = useState<LatLngExpression[]>([]);
 
   useEffect(() => {
-    (async() => {
-      const response = await axios.post(`http://localhost:3001/route`, coord);
-      setRoute(response.data);
-    })()
+    if (coord && coord.start && coord.end) {
+      (async () => {
+        const response = await axios.post(`http://localhost:3001/route`, coord);
+        setRoute(response.data);
+      })();
+    }
   }, [coord]);
 
   return [route, setCoord] as const;
